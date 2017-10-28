a1f75f1a881cf221bda7ac4f691242e5ff9eeef4
Liz Burton <liz.burton147@gmail.com>
2017-10-28 21:49:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,22 +9,73 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+  var itemPrice = Math.floor(Math.random() * 100 );
+  var itemObject = { [itemName]: itemPrice };
+  cart.push(itemObject);
+  console.log(itemName + " has been added to your cart.");
+  return cart;
 }
 
+
 function viewCart() {
-  // write your code here
+  var cartContentsMessage = "In your cart, you have ";
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  } else {
+    for (let i=0; i<cart.length; i++) {
+      var itemName = Object.keys(cart[i]);
+      var itemPrice = cart[i][itemName];
+      cartContentsMessage += itemName + " at $" + itemPrice;
+      if (i === cart.length - 1) {
+        cartContentsMessage +="."
+      } else if (i === cart.length - 2) {
+         if (cart.length > 2) {
+           cartContentsMessage += ", and ";
+         } else {
+           cartContentsMessage += " and ";
+         }
+      } else {
+        cartContentsMessage += ", "
+      }
+    }
+    console.log(cartContentsMessage);
+  }
 }
 
 function total() {
-  // write your code here
+  var totalPrice = 0;
+  for (let i=0; i<cart.length; i++) {
+    var itemName = Object.keys(cart[i]);
+    totalPrice += cart[i][itemName];
+  }
+  console.log(totalPrice);
+  return totalPrice;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i=0; i<cart.length; i++) {
+    console.log(cart[i].hasOwnProperty(item));
+    if ( cart[i].hasOwnProperty(item) ) {
+        delete cart[i];
+        console.log(cart);
+        return cart;
+    }
+  }
+  console.log("That item is not in your cart.")
 }
 
 function placeOrder(cardNumber) {
   // write your code here
 }
+
+//Code Testing
+
+addToCart('bananas');
+addToCart('strawberries');
+addToCart('apples');
+addToCart('pineapple');
+viewCart();
+total();
+console.log(cart);
+removeFromCart('guava');
