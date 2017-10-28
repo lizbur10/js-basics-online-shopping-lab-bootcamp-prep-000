2c63a9b9322a248e411616d2d81aed482c6556e9
Liz Burton <liz.burton147@gmail.com>
2017-10-28 22:00:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardnumber) {
+
+  }
+  else {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
 }
 
 //Code Testing
